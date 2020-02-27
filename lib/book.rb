1f056e8954665cc9df776e2a5a7030caae0f916d
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-02-27 00:43:00 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -5,19 +5,19 @@ end
 def title
   @title
 end
-def author(author)
+def author=(author)
   @author = author 
 end
 def author
   @author
 end
-def page_count(num)
+def page_count=(num)
   @page_count = page_count
 end
 def page_count
   @page_count
 end
-def genre(genre)
+def genre=(genre)
   @genre = genre
 end
 def genre
